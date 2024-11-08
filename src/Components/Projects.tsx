7eef0ec5ae0fb5af68e248d28eb70667fa1069f2
Francisco Diaz <33507754+diaz-francisco@users.noreply.github.com>
2024-11-08 14:18:20 -0600

    Update Projects.tsx

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -4,13 +4,13 @@ export default function Projects() {
       <div className="projects fade-in">
         <ul>
           <li>
-            <a href="https://mrcheks.github.io/FilmSearch/" target="_blank">
+            <a href="https://diaz-francisco.github.io/FilmSearch/" target="_blank">
               <h1>Cinnemon</h1>
             </a>
             <p>Cinnemon is a React application similar to IMDB</p>
           </li>
           <li>
-            <a href="https://github.com/MrCheks/GuessTheFlag" target="_blank">
+            <a href="https://github.com/diaz-francisco/GuessTheFlag" target="_blank">
               <h1 style={{ fontSize: "25px" }}>Flagcisco</h1>
             </a>
             <p>
