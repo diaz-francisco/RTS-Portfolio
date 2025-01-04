d1e7376f344a241ea0a436d5a9159607bcfe4c9f
Francisco Diaz <33507754+diaz-francisco@users.noreply.github.com>
2025-01-03 18:30:50 -0600

    Update AboutMe.tsx

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -4,11 +4,11 @@ export default function AboutMe() {
       <div className="aboutMe fade-in">
         <h2>What I'm currently reading</h2>
         <p>Blood Meridian, Cormac Mc Carthy</p>
-        <p>A Game of Thrones, George R.R. Martin</p>
+        <p>House of Leaves, Mark Z. Danielewski</p>
         <h2>What's Next?</h2>
         <p>
           Right now I'm building projects and further improving with back-end technologies such as Node.js, Deno 2.0,
-          Express, MongoDB and Mongoose.
+          Express, MongoDB, Mongoose and Docker.
         </p>
         <h2>Who am I?</h2>
         <p>
